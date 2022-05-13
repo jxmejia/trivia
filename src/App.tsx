ac32783a733044cfee62d8e92c30982960343d70
jxmejia <kunamo@gmail.com>
2022-05-13 17:11:04 -0400

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Global } from "@emotion/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import globalStyles from "./styles/global";
+import { QuestionsContextProvider } from "./context";
+import { Home, Quiz, Results } from "./views";
 
 function App() {
+  // TODO: Add error boundary https://www.npmjs.com/package/react-error-boundary
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global styles={globalStyles} />
+      <QuestionsContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/results" element={<Results />} />
+          </Routes>
+        </BrowserRouter>
+      </QuestionsContextProvider>
+    </>
   );
 }
 
