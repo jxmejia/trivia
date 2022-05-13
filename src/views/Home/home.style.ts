6f13538efddd7bfba9723cc7e9f7c4a7d872d0d7
jxmejia <kunamo@gmail.com>
2022-05-13 16:29:58 -0400

    Add home view

---
+++ src/views/Home/home.style.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const TitleContainer = styled.div`
+  margin: 50px;
+`;
+
+export const TextContainer = styled.div`
+  margin: 25px;
+`;
+
+export const ButtonContainer = styled.div`
+  margin: 25px;
+`;
---
+++ src/views/Home/home.view.tsx
@@ -0,0 +1,39 @@
+import { memo, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Layout, Text, Title } from "../../components";
+import { QuestionsContext } from "../../context";
+import { ButtonContainer, TextContainer, TitleContainer } from "./home.style";
+
+function Home() {
+  const navigate = useNavigate();
+  const { questions, getQuestions } = useContext(QuestionsContext);
+
+  useEffect(() => {
+    if (questions?.length) {
+      navigate("/quiz");
+    }
+  }, [questions, getQuestions]);
+
+  const handleClick = () => getQuestions?.();
+
+  return (
+    <Layout>
+      <TitleContainer>
+        <Title>Welcome to the trivia challenge</Title>
+      </TitleContainer>
+      <TextContainer>
+        <div>
+          <Text>You will be presented with 10 True or False questions.</Text>
+        </div>
+        <div>
+          <Text>Can you score 100%?</Text>
+        </div>
+      </TextContainer>
+      <ButtonContainer>
+        <Button onClick={handleClick}>Begin</Button>
+      </ButtonContainer>
+    </Layout>
+  );
+}
+
+export default memo(Home);
