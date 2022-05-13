e8d6b1b023bf9bfd739d2331221746497b64884e
jxmejia <kunamo@gmail.com>
2022-05-13 15:59:11 -0400

    Add questions mock

---
+++ src/mocks/questions.mock.ts
@@ -0,0 +1,87 @@
+export const questionsMock = [
+  {
+    category: "Vehicles",
+    type: "boolean",
+    difficulty: "hard",
+    question:
+      "In 1993 Swedish car manufacturer Saab experimented with replacing the steering wheel with a joystick in a Saab 9000.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "History",
+    type: "boolean",
+    difficulty: "hard",
+    question: "Joseph Stalin&#039;s real name was Ioseb Bessarionis dze Dzugashvili.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science & Nature",
+    type: "boolean",
+    difficulty: "hard",
+    question:
+      "It was once believed that injecting shark cartilage into people would prevent them from contracting cancer.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Music",
+    type: "boolean",
+    difficulty: "hard",
+    question:
+      "The song &quot;Mystery Train&quot; was released by artist &quot;Little Junior&#039;s Blue Flames&quot; in 1953.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Politics",
+    type: "boolean",
+    difficulty: "hard",
+    question: "Joko Widodo has appeared in the cover of a TIME magazine.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Music",
+    type: "boolean",
+    difficulty: "hard",
+    question: "The band STRFKR was also briefly known as Pyramiddd.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science & Nature",
+    type: "boolean",
+    difficulty: "hard",
+    question: "The value of one Calorie is different than the value of one calorie.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Music",
+    type: "boolean",
+    difficulty: "hard",
+    question:
+      "Pete Townshend&#039;s solo album, &quot;White City: A Novel&quot;, is set in the metropolitan area of Chicago.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Mythology",
+    type: "boolean",
+    difficulty: "hard",
+    question: "Janus was the Roman god of doorways and passageways.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Entertainment: Japanese Anime & Manga",
+    type: "boolean",
+    difficulty: "hard",
+    question:
+      "The protagonist in &quot;Humanity Has Declined&quot; has no discernable name and is simply referred to as &#039;I&#039; for most of the series.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+];
